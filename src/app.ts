5bf0d5a0bb47459beeb247cad28edd0a9ab95a8b
TTiip <2711430287@qq.com>
2022-03-22 11:44:46 +0800

    feat: 设计路由去调用不同的接口。实现 koa get post请求 传递参数等

--- src/app.ts
+++ src/app.ts
@@ -10,7 +10,7 @@ const app = new Koa()
 // 总结： 在加了router.allowedMethods()中间件情况下
 // 如果接口是get请求，而前端使用post请求，会返回405 Method Not Allowed ，提示方法不被允许
 // 并在响应头有添加允许的请求方式；而在不加这个中间件这种情况下，则会返回 404 Not Found找不到请求地址
-// 并且响应头没有添加允许的请求方式
+// 并且响应头没有添加允许的请求方式 。
 
 const session_signed_key = ['哈塞给']  // 这个是配合signed属性的签名key
 const session_config = {
