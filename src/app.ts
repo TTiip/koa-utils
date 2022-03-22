c8a019ae87c53c9b01f8e12962afb80b0adbaba8
TTiip <2711430287@qq.com>
2022-03-22 11:35:11 +0800

    feat: 设计路由去吊用不同的接口，koa get post请求 传递参数等

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,40 @@
 import Koa from 'koa'
+import KoaSession from 'koa-session'
+import bodyParser from 'koa-bodyparser'
+import cors from 'koa2-cors'
 import { router } from './routers'
 
 const port = 3000
 const app = new Koa()
 
+// 总结： 在加了router.allowedMethods()中间件情况下
+// 如果接口是get请求，而前端使用post请求，会返回405 Method Not Allowed ，提示方法不被允许
+// 并在响应头有添加允许的请求方式；而在不加这个中间件这种情况下，则会返回 404 Not Found找不到请求地址
+// 并且响应头没有添加允许的请求方式 。
+
+const session_signed_key = ['哈塞给']  // 这个是配合signed属性的签名key
+const session_config = {
+  key: 'koa:sess', /**  cookie的key。 (默认是 koa:sess) */
+  maxAge: 24 * 60 * 60 * 1000,   /**  session 过期时间，以毫秒ms为单位计算 。*/
+  autoCommit: true, /** 自动提交到响应头。(默认是 true) */
+  overwrite: true, /** 是否允许重写 。(默认是 true) */
+  httpOnly: true, /** 是否设置HttpOnly，如果在Cookie中设置了"HttpOnly"属性，那么通过程序(JS脚本、Applet等)将无法读取到Cookie信息，这样能有效的防止XSS攻击。  (默认 true) */
+  signed: true, /** 是否签名。(默认是 true) */
+  rolling: true, /** 是否每次响应时刷新Session的有效期。(默认是 false) */
+  renew: false /** 是否在Session快过期时刷新Session的有效期。(默认是 false) */
+}
+
+app.keys = session_signed_key
+
+const session = KoaSession(session_config, app)
+
 ;[
-  router.routes(),
-  router.allowedMethods()
+  session, // session 相关信息 设置登陆时间等等
+  // cors({ credentials: true }),
+  cors(), // 允许跨域
+  bodyParser(), // 解析 post 传递的参数，放到请求 body 里面， 必须写在路由前面！！！
+  router.routes(), // 使用路由
+  router.allowedMethods(), // 一定放在路由使用之后！！！ 此时根据ctx.status设置response响应头
 ].map(item => app.use(item))
 
 app.listen(port, () => console.log(`----- serve start at port ${port} -----`))
--- src/routers/index.ts
+++ src/routers/index.ts
@@ -1,11 +1,82 @@
+import fs from 'fs'
+import { resolve } from 'path'
+import { getResponseData } from '../interface'
+import { Context } from 'koa'
 import koaRouter from 'koa-router'
 
+import { DellAnalyzer } from '../analyzer'
+import { Crowller } from '../crowller'
+
 const router = new koaRouter()
-router.get('/user', async (ctx) => {
-	ctx.body="这是用户操作首页"
+
+const writePath = resolve(__dirname, '../index.json')
+
+router.get('/', async (ctx: Context) => {
+	const isLogin = ctx.session && ctx.session.login
+	if (isLogin) {
+		ctx.body = `
+			<html>
+				<body>
+					<a href='/getData'>爬取内容</a>
+					<a href='/showData'>展示内容</a>
+					<a href='/logout'>退出</a>
+				</body>
+			</html>
+		`
+	} else {
+		ctx.body = `
+			<html>
+				<body>
+					<form method="post" action="/login">
+						账号随意，密码123。
+						<br/ >
+						账号：<input type="text" name="password" />
+						<br/ >
+						密码：<input type="password" name="password" />
+						<button>登陆</button>
+					</form>
+				</body>
+			</html>
+		`
+	}
 })
-router.get('/user/register', async (ctx) => {
-	ctx.body="用户注册接口"
+
+router.post('/login', async (ctx: Context) => {
+	const isLogin = ctx.session && ctx.session.login
+	const { password } = ctx.request.body
+
+	if (isLogin) {
+		ctx.body = getResponseData(false, '已经登陆过')
+	} else {
+		if (ctx.session) {
+			if (password[1] === '123') {
+				ctx.session.login = true
+				ctx.session.userInfo = { name: password[0], password: password[1] }
+				ctx.body = getResponseData(true)
+			} else {
+				ctx.body = getResponseData(false, '密码错误！')
+			}
+		} else {
+			ctx.body = getResponseData(false, '登陆失败')
+		}
+	}
+})
+
+router.get('/getData', async (ctx: Context) => {
+	const url: string = 'https://www.zhihu.com/billboard'
+
+	const dell = new DellAnalyzer(url)
+	new Crowller(writePath, dell)
+	ctx.body = getResponseData(true)
+})
+
+router.get('/showData', async (ctx: Context) => {
+	try {
+		const result = fs.readFileSync(writePath, 'utf-8')
+		ctx.body = getResponseData(JSON.parse(result))
+	} catch (e) {
+		ctx.body = getResponseData(false, '数据不存在')
+	}
 })
 
 export {
