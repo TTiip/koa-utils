ed02725a5161f6b65b67b7c22e22bc07bf4606d2
TTiip <2711430287@qq.com>
2022-03-22 11:44:57 +0800

    feat: 设计路由去调用不同的接口。实现 koa get post请求 传递参数等

--- src/routers/index.ts
+++ src/routers/index.ts
@@ -8,7 +8,6 @@ import { zhihuHotAnalyze } from '../analyzer'
 import { Crowller } from '../crowller'
 
 const router = new koaRouter()
-
 const writePath = resolve(__dirname, '../index.json')
 
 router.get('/', async (ctx: Context) => {
